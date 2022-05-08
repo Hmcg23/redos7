626e5f5f3655134237ef5a0f9f856fbd69322993
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-05-07 20:38:06 -0700

    add fonts and colors

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,7 +6,9 @@ function HomePage() {
             <div className='header'>
                 <h1>a new place for <span className='red-text'>RedOS 7</span></h1> 
             </div>
-            <h2>Our new website helps members to collaborate & understand each other better. It also helps guests to apply easier.</h2>          
+            <h2>Our new website helps members to collaborate & understand each other better. It also helps guests to apply easier.</h2>   
+            <button>Apply</button>
+            <button>Learn More</button>
       </section>
   );
 }
