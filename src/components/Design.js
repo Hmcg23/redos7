36526ef900ca0ea8b20f578c652a91fd11c00591
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-05-11 14:39:07 -0700

    add design page html and css

--- src/components/Design.js
+++ src/components/Design.js
@@ -2,20 +2,49 @@ import { useEffect } from 'react';
 import '../App.css';
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useNavigate } from 'react-router-dom';
+import redos7logo from '../images/redos7logo.png'
 
 function Design() {
-  const navigate = useNavigate();
   
     useEffect(() => {
         AOS.init();
         AOS.refresh(); 
       }, []);
   return (
-      <section data-aos='fade-up' data-aos-duration='1000'>
+      <section data-aos='fade-up' data-aos-duration='1000' className='design-page'>
         <h1 className='page-h1'>Design</h1>
-        <h1>This page is still in progress, more coming soon!</h1>
-        <button onClick={(e) => { navigate('/') }}>Go Home</button>        
+        <div className='design-page-grid'>
+          <div className='design-page-box-branding'>
+            <img src={redos7logo} alt='redos7 logo' />
+            <div className='design-page-text'>
+              <h2>Branding</h2>
+              <h3>Our branding is who we are</h3>
+            </div>
+          </div>
+          <div className='design-page-box-colors'>
+            <img src={redos7logo} alt='redos7 logo' />
+            <div className='design-page-text'>
+              <h2>Colors</h2>
+              <h3>Our colors is what defines us</h3>
+            </div>
+          </div>
+          <div className='design-page-box-typo'>
+            <img src={redos7logo} alt='redos7 logo' />
+            <div className='design-page-text'>
+              <h2>Typography</h2>
+              <h3>Fonts that represent us</h3>              
+            </div>
+          </div>
+          <div className='design-page-box-graphics'>
+            <img src={redos7logo} alt='redos7 logo' />
+            <div className='design-page-text'>
+              <h2>Graphics</h2>
+              <h3>Icons that are consistent</h3>              
+            </div>
+
+          </div>    
+        </div>
+     
       </section>
   );
 }
